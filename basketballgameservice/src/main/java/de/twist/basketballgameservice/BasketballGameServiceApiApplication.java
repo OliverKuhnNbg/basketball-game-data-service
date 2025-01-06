6390436763e90d878a03bef806606ffa816ec6bd
OliverKuhnNbg <oliver.o.kuhn@gmail.com>
2025-01-06 14:47:21 +0100

    init base the project

---
+++ basketballgameservice/src/main/java/de/twist/basketballgameservice/BasketballGameServiceApiApplication.java
@@ -0,0 +1,13 @@
+package de.twist.basketballgameservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasketballGameServiceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasketballGameServiceApiApplication.class, args);
+	}
+
+}
